d75a30b82e534955c101862c6831bf590d8ede8d
purpletianjing <TianjingSun@outlook.com>
2016-03-10 13:16:18 +0800

    delete code

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,11 +1,7 @@
 'use strict';
 
 function collect_all_even(collection) {
-  var collection_result = [];
-  collection_result = collection.filter(function(ele){
-    return (ele % 2 == 0);
-  });
-  return collection_result;
+  //在此处实现函数过程
 }
 
 module.exports = collect_all_even;
