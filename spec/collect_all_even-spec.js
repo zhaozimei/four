4ca130161cee5bdf848993ed46154f3e9afdc5bd
purpletianjing <TianjingSun@outlook.com>
2016-03-23 10:38:57 +0800

    Update collect_all_even-spec.js

--- spec/collect_all_even-spec.js
+++ spec/collect_all_even-spec.js
@@ -13,7 +13,7 @@ describe('practice-1-4', function() {
   var collection_c = [
     {key: "a"}, {key: "j"}, {key: "f"}, {key: "t"}, {key: "n"}, {key: "c"}, {key: "f"}, {key: "e"}, {key: "d"}
   ];
-  var collection_c = {value: ["a", "d", "e", "f"]};
+  var collection_d = {value: ["a", "d", "e", "f"]};
 
   it("选出A集合中元素的key属性，跟B对象中value属性中的元素相同的元素", function() {
     var result = collect_same_elements(collection_a, collection_b);
@@ -22,7 +22,7 @@ describe('practice-1-4', function() {
   });
   
   it("选出C集合中元素的key属性，跟D对象中value属性中的元素相同的元素", function() {
-    var result = collect_same_elements(collection_a, collection_b);
+    var result = collect_same_elements(collection_c, collection_d);
 
     expect(result).toEqual(["a", "f", "f", "e", "d"]);
   });
