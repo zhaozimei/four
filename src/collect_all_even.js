c52a5740f6cd224f744a7fbb1b76ae322f84b03e
zhaozimei <1140173180@qq.com>
2016-05-29 21:28:38 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,17 @@
 
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+  var arr = [];
+	var k = 0;
+	for(var i=0; i<collection_a.length; i++){
+	    k = collection_a[i].key
+	   	for(var j=0; j<object_b.value.length; j++){
+			     if(object_b.value[j] == k){
+				    arr.push(k)
+			      }
+		   }
+	}	
+return arr;
+
 }
 
 module.exports = collect_same_elements;
